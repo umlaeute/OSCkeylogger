db9777761248e3d25622e4e2a06ace4156c28c53
IOhannes m zmölnig <zmoelnig@iem.at>
2012-05-21 19:33:54 +0200

    moved print code into separate function
    and removed all members that are not available on POSIX

--- kb2osc.py
+++ kb2osc.py
@@ -4,24 +4,21 @@
 import sys
 import hooklib, osc
 
-def OnKeyboardEvent(event):
+def printevent(event):
     print 'MessageName:',event.MessageName
-#    print 'Message:',event.Message
-#    print 'Time:',event.Time
     print 'Window:',event.Window
     print 'WindowName:',event.WindowName
     print 'Ascii:', event.Ascii, chr(event.Ascii)
     print 'Key:', event.Key
     print 'KeyID:', event.KeyID
     print 'ScanCode:', event.ScanCode
-#    print 'Extended:', event.Extended
-#    print 'Injected:', event.Injected
-#    print 'Alt', event.Alt
-#    print 'Transition', event.Transition
     print '---'
 
     return True
  
+def OnKeyboardEvent(event):
+	printevent(event)
+	return True
 
 def main(script, port=6666, host="localhost"):
         port_=int(port)
