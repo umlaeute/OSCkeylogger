f21b5c6c85ef258ceca4ad65be5bd098bb2a0f38
IOhannes m zmölnig <zmoelnig@iem.at>
2012-05-21 20:02:50 +0200

    logging to OSC

--- kb2osc.py
+++ kb2osc.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 # vim: set fileencoding=UTF-8 :
 
+"""keylogger that send events via OSC.
+each keypress generates an event like:
+  /keylogger/<window>/<keyname> <bool:down>
+"""
+
 import sys
 import hooklib, osc
+
+osc.init()
+Host="localhost"
+Port=6666
 
 def printevent(event):
     print 'MessageName:',event.MessageName
@@ -15,16 +24,28 @@ def printevent(event):
     print '---'
 
     return True
+
+def sendevent(event, down):
+	bundle=osc.createBundle()
+	osc.appendToBundle(bundle, "/keylogger/window/"+event.Window, [event.Key, down])
+	osc.appendToBundle(bundle, "/keylogger/WindowName/"+event.WindowName, [event.Key, down])
+	osc.sendBundle(bundle, Host, Port)
+	
  
 def OnKeyboardEvent(event):
-	printevent(event)
+	#printevent(event)
+	if("key down"==event.MessageName):
+		sendevent(event, 1)
+	elif("key up"==event.MessageName):
+		sendevent(event, 0)
 	return True
 
 def main(script, port=6666, host="localhost"):
-        port_=int(port)
-	osc.init()
+	Host=host
+        Port=int(port)
 	hm=hooklib.HookManager()
 	hm.KeyDown = OnKeyboardEvent
+	hm.KeyUp = OnKeyboardEvent
 	hm.HookKeyboard()
 	hm.start()
 
